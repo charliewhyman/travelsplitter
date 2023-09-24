c47c66fd12c3bb8862226500d97ddb2432ddbe29
Charlie Whyman <65013284+charliewhyman@users.noreply.github.com>
2023-09-24 13:18:27 +0200

    Added supabase helper file

---
+++ app/lib/supabase.ts
@@ -0,0 +1,30 @@
+import 'react-native-url-polyfill/auto'
+import * as SecureStore from 'expo-secure-store'
+import { createClient } from '@supabase/supabase-js'
+
+
+// fromhttps://supabase.com/docs/guides/getting-started/tutorials/with-expo
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key)
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value)
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key)
+  },
+}
+
+const supabaseUrl:string = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey:string = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
