013bcba7c9d4e80c56a3ccae47df4a70e730937d
Charlie Whyman <65013284+charliewhyman@users.noreply.github.com>
2023-12-04 23:14:03 +0000

    Added router.replaces back to index

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,28 @@
 import { useEffect, useState } from "react";
 import { supabase } from "./lib/supabase";
 import { Session } from "@supabase/supabase-js";
+import { router } from "expo-router";
 
 export default function IndexPage() {
   const [session, setSession] = useState<Session | null>(null)
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
+      if (session) {
+        router.replace('/(tabs)/home')
+      } else {
+        console.log('no user')
+        router.replace('/(auth)/login')
+      }
     })
 
     supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
+        if (session) {
+            router.replace('/(tabs)/home');
+        } else {
+            router.replace('/(auth)/login');
+        }
+
     })
-  }, []);
-}
+  }, [])
+};
