4e7556e67afe54a8f5f013b85db6412c24060189
Charlie Whyman <65013284+charliewhyman@users.noreply.github.com>
2023-12-13 11:56:32 +0000

    Updated Groups component to correctly display groups and links

--- components/Groups.tsx
+++ components/Groups.tsx
@@ -1,73 +1,37 @@
 import React, { useEffect, useState } from 'react'
-import { supabase } from '../app/lib/supabase';
 import { View, Text } from './Themed';
-import { Link, router, useNavigation, useFocusEffect } from 'expo-router';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Link, useNavigation } from 'expo-router';
 import { Session } from '@supabase/supabase-js';
+import { fetchSession, getGroups } from '../app/helpers/groupHandler';
 
-export default function Groups() {
+export default function Groups() {4
+  const [loading, setLoading] = useState(true);
   const [groups, setGroups] = useState<any[]>([]); // TODO add actual type
   const [session, setSession] = useState<Session | null>(null)
   const navigation = useNavigation();
 
-  const fetchGroups = async (sessionData: Session | null) => {
-
-    try {
-      if (!sessionData?.user) {
-        router.replace('/(auth)/login')
-        throw new Error('No user on the session! (Groups.tsx)')
-      }
-
-      let user = sessionData.user.id
-      let { data: groupsData, error } = await supabase
-        .from('group_members')
-        .select(`
-        id,
-        groups (
-          id,
-          name,
-          slug
-        )
-        `).eq('member_id', sessionData.user.id);
-
-      if (error) {
-        console.error('Error fetching groups: ', error.message);
-      } else {
-        console.log(groupsData)
-        setGroups(groupsData || []);
-      }
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error('Error fetching groups: ', error.message);
-      }
-    } 
-  }
-  
-
-  useFocusEffect(() => {
-    if (session) fetchGroups(session);
-
-  })
-
   useEffect(() => {
-    
-    if (session) fetchGroups(session);
-    
-  }, [session]); // Run this effect only once, similar to componentDidMount
+    async function fetchData() {
+      const session = await fetchSession();
+      if (session) {
+        await getGroups(session, setGroups, setLoading);
+        setSession(session);
+      }
+    }
 
+    fetchData();
+    console.log(session,groups)
+  }, [navigation]);
+  
   return (
     <View>
       <Text>My Groups</Text>
-      {groups.map((group) => (
-        <Link key={group.id} href={{
+      {groups.map((group, index) => (
+        <Link key={index} href={{
           pathname: `/group/${group.slug}`,
           params: { id: group.id }
-        }}
-        
-        asChild>
-          <TouchableOpacity>
-            <Text key={group.id}>{group.name} </Text>
-          </TouchableOpacity>
+        }}>
+          {group}
         </Link>
       ))}
     </View>
